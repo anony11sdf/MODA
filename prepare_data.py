3eac3d5ab93ffa26ba9061dcce78d4e72f587e25
anony11sdf <159478732+anony11sdf@users.noreply.github.com>
2024-02-08 14:06:18 -0500

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -6,91 +6,7 @@ import torch
 import random
 
 
-# def get_subtraj(trajectory, number_transition, start_index):
     
-    
-    
-#     subtraj = []
-#     subtraj = trajectory[start_index]   ## (s1, a1, r1)
-#     index = start_index + 1
-    
-    
-#     for ii in range(number_transition-1):  ### (s1, a1, r1)+(s2, a2, r2)+....
-#         subtraj = subtraj + trajectory[index]
-#         index = index + 1
-    
-#     #subtraj += trajectory[index][0:125]  #### s'
-    
-#     tmp = trajectory[index][0:125]
-#     subtraj = subtraj + tmp #### s'
-    
-    
-    
-    
-#     return subtraj
-
-
-
-
-# def get_triples(aim_driver :int, number_transition :int):
-    
-#     cnt = 0
-    
-#     file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
-#     tagged_traj = pickle.load(file)
-#     file.close()
-    
-#     tuple_list = []
-#     for trajectory in tagged_traj[aim_driver][1]: 
-        
-#         cnt += 1
-#         #print(cnt)
-
-#         if len(trajectory)-1 <= number_transition:  ####保证至少一个anchor一个positive
-#             continue
-#         else:
-#             for i in range(len(trajectory)-1-number_transition): ####这个1是滑动间隔，可以考虑设为hyperparameter
-                
-                
-                
-#                 if i >2:
-#                     break
-                
-                
-                
-#                 anchor = get_subtraj(trajectory, number_transition, i)
-#                 positive = get_subtraj(trajectory, number_transition, i+1)
-                
-#                 ###get negative
-                
-#                 ct1=0
-                
-#                 for driver, trajs in tagged_traj: 
-                    
-#                     ct1+=1
-#                     if ct1>4:
-#                         break
-                    
-#                     if driver != aim_driver:
-#                         for j in trajs: ####j 代表一个trajectory
-                            
-#                             if i + number_transition < len(j):
-                                
-#                                 negative = get_subtraj(j, number_transition, i)
-#                                 tuple = (anchor, positive, negative)
-                                
-#                                 if len(anchor)!= 252 + 127 * (number_transition-1) or len(positive)!= 252 + 127 * (number_transition-1) or len(negative)!= 252 + 127 * (number_transition-1):
-#                                     print('error', len(anchor),' ',len(positive),' ',len(negative))
-                                    
-                                    
-#                                 tuple_list.append(tuple)
-#     return tuple_list
-            
-        
-    
-    
-    
-
 
 
 def get_subtraj(trajectory, number_transition, start_index):
@@ -102,13 +18,12 @@ def get_subtraj(trajectory, number_transition, start_index):
         return tp
     
     subtraj = []
-    subtraj = trajectory[start_index]   ## (s1, a1, r1)
+    subtraj = trajectory[start_index]   
     index = start_index + 1
     
     
-    for ii in range(0, number_transition-1):  ### (s1, a1, r1)+(s2, a2, r2)+....
+    for ii in range(0, number_transition-1):  
         subtraj = subtraj + trajectory[index]
-        #print('error3')
         index = index + 1
     
     
@@ -132,45 +47,38 @@ def get_triples(aim_driver :int, number_transition :int, filter_out:int, used_dr
     
     #cnt = 0
     
-    file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
+    file = open()
     tagged_traj1 = pickle.load(file)
     file.close()
     
     tuple_list = []
-    for trajectory in tagged_traj1[aim_driver][1]: #### dont need last trajectory
-        
-        #cnt += 1
-        #print(cnt)
-        #ct =0
+    for trajectory in tagged_traj1[aim_driver][1]: 
         
 
-        if len(trajectory)-1 <= number_transition:  ####保证至少一个anchor一个positive
+        
+
+        if len(trajectory)-1 <= number_transition:  
             continue
         else:
-            for i in range(len(trajectory)-1-number_transition):   ##i is start idx
+            for i in range(len(trajectory)-1-number_transition): 
                 
                 
-                if i>move_size:  ###同一个trajectory 向右滑动的最大距离
+                if i>move_size:  
                     break
-                
-                
                 
                 anchor = get_subtraj(trajectory, number_transition, i)
                 anchor = torch.tensor(anchor, dtype=torch.float32)
+
                 
-                # if i + slide_length + number_transition > len(trajectory):
-                #     break
-                
-                positive = get_subtraj(trajectory, number_transition, i + slide_length)  ####slide_length 这个1是滑动间隔，可以考虑设为hyperparameter
+                positive = get_subtraj(trajectory, number_transition, i + slide_length)  
                 if len(positive) == 0:
                     break
                     
                 
                 positive = torch.tensor(positive, dtype=torch.float32)
-                ###get negative
-                
 
-                ct1 = 0 ##### control negative drivers' number
+
+                ct1 = 0
                 
                 
                 for k in range(len(tagged_traj1)):
@@ -178,22 +86,15 @@ def get_triples(aim_driver :int, number_transition :int, filter_out:int, used_dr
                     ct1+=1
                     if ct1>=used_driver_size:
                         break
-                    
-                    
-                    #print('k',k)
-                    
-                    
-                    
+
                     if k != aim_driver:
                         
                         
-                        
-                        
+                                            
                         for j in tagged_traj1[k][1]:
-                            #print('len j',len(j))
+
                             
-                            
-                            random_integer = random.randint(0, 100)  #### 在当前司机下，限制选择的trajectory数量
+                            random_integer = random.randint(0, 100)  
                             
                             
                             if i + number_transition + 1 < len(j) and random_integer > filter_out:
@@ -216,9 +117,9 @@ def get_triples(aim_driver :int, number_transition :int, filter_out:int, used_dr
         
 def get_anchor(aim_driver :int, number_transition :int, filter_out:int, used_driver_size:int, slide_length: int, move_size:int):
     
-    #cnt = 0
+
     
-    file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
+   
     tagged_traj1 = pickle.load(file)
     file.close()
     
@@ -226,7 +127,7 @@ def get_anchor(aim_driver :int, number_transition :int, filter_out:int, used_dri
     
     for driver_idx in range(used_driver_size):
         
-        if driver_idx != aim_driver:   #####跳过not目标司机
+        if driver_idx != aim_driver:   
             continue
         
         else:
@@ -235,19 +136,16 @@ def get_anchor(aim_driver :int, number_transition :int, filter_out:int, used_dri
             for trajectory in tagged_traj1[driver_idx][1]: #### 
                 
 
-                if len(trajectory)-1 <= number_transition:  ####保证至少一个sub-trajectory
+                if len(trajectory)-1 <= number_transition: 
                     continue
                 else:
                     for i in range(len(trajectory)-1-number_transition):
                         
                         
-                        if i > move_size:  ###同一个trajectory 向右滑动的最大距离
+                        if i > move_size: 
                             break
                         
                         anchor = get_subtraj(trajectory, number_transition, i)
-                        #anchor = torch.tensor(anchor, dtype=torch.float32)
-                        
-                        #anchor_list.append(anchor)
                         if len(anchor)!= 252 + 127 * (number_transition-1) :
                             print('error', len(anchor))
                             continue
@@ -265,7 +163,7 @@ def get_negative(aim_driver :int, number_transition :int, filter_out:int, used_d
     
     
         
-    file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
+   
     tagged_traj1 = pickle.load(file)
     file.close()
     
@@ -276,20 +174,19 @@ def get_negative(aim_driver :int, number_transition :int, filter_out:int, used_d
         if ii == aim_driver:
             continue
         
-        for trajectory in tagged_traj1[ii][1]: #### dont need last trajectory
+        for trajectory in tagged_traj1[ii][1]: 
             
 
-            if len(trajectory)-1 <= number_transition:  ####保证至少一个anchor一个positive
+            if len(trajectory)-1 <= number_transition:  
                 continue
             else:
                 for i in range(len(trajectory)-1-number_transition):
                     
                     
-                    if i > move_size:  ###同一个trajectory 向右滑动的最大距离
+                    if i > move_size:  
                         break
                     
                     negative = get_subtraj(trajectory, number_transition, i)
-                    #anchor = torch.tensor(anchor, dtype=torch.float32)
                     
                     
                     if len(negative)!= 252 + 127 * (number_transition-1) :
@@ -299,203 +196,4 @@ def get_negative(aim_driver :int, number_transition :int, filter_out:int, used_d
                     negative_list.append(negative)
                           
     return negative_list  
-    
-    
-    #cnt = 0
-    
-    
-    
-        
-def get_negative_V0(aim_driver :int, number_transition :int, filter_out:int, used_driver_size:int, slide_length: int, move_size:int): ####目前广泛使用
-    
-    
-        
-    file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
-    tagged_traj1 = pickle.load(file)
-    file.close()
-    
-    negative_list = []
-    
-    
-    for trajectory in tagged_traj1[aim_driver][1]: #### dont need last trajectory
-        
-
-        if len(trajectory)-1 <= number_transition:  ####保证至少一个anchor一个positive
-            continue
-        else:
-            for i in range(len(trajectory)-1-number_transition):
-                
-                
-                if i > move_size:  ###同一个trajectory 向右滑动的最大距离
-                    break
-                
-                negative = get_subtraj(trajectory, number_transition, i)
-                #anchor = torch.tensor(anchor, dtype=torch.float32)
-                
-                
-                if len(negative)!= 252 + 127 * (number_transition-1) :
-                    print('error', len(negative))
-                    continue
-                    
-                negative_list.append(negative)
-                          
-    return negative_list  
-    
-    
-    
-    
-    
-    # file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
-    # tagged_traj1 = pickle.load(file)
-    # file.close()
-    
-    # negative_list = []
-    # for trajectory in tagged_traj1[aim_driver][1]: #### dont need last trajectory
-        
-    #     if len(trajectory)-1 <= number_transition:  ####保证至少一个anchor一个positive
-    #         continue
-    #     else:
-    #         for i in range(len(trajectory)-1-number_transition):
-                
-                
-    #             if i>move_size:  ###同一个trajectory 向右滑动的最大距离
-    #                 break
-                              
-    #             #anchor = get_subtraj(trajectory, number_transition, i)
-    #             #anchor = torch.tensor(anchor, dtype=torch.float32)
-
-    #             ct1 = 0 ##### control negative drivers' number
-                
-                
-    #             for k in range(len(tagged_traj1)):
-                    
-    #                 ct1+=1
-    #                 if ct1>=used_driver_size:
-    #                     break
- 
-                    
-    #                 if k != aim_driver:
-                        
-                         
-    #                     for j in tagged_traj1[k][1]:
-    #                         #print('len j',len(j))
-                            
-                            
-    #                         random_integer = random.randint(0, 100)  #### 在当前司机下，限制选择的trajectory数量
-                            
-                            
-    #                         if i + number_transition + 1 < len(j) and random_integer > filter_out:
-                                
-    #                             negative = get_subtraj(j, number_transition, i)
-    #                             #negative = torch.tensor(negative, dtype=torch.float32)
-                                
-                                
-    #                             if len(negative)!= 252 + 127 * (number_transition-1):
-    #                                 print('error',len(negative))
-                                    
-                                    
-    #                             negative_list.append(negative)
-                                
-    # return negative_list         
-        
-        
-        
-        
-        
-        
-        
-            
-            
-def get_triples_V2(aim_driver :int, number_transition :int, filter_out:int, used_driver_size:int, slide_length: int, move_size:int): ########更节省空间，或许用不到...
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #cnt = 0
-    
-    file = open('/home/xinbo/project1/data/reward_traj.pkl', 'rb')
-    tagged_traj1 = pickle.load(file)
-    file.close()
-    
-    tuple_list = []
-    for trajectory in tagged_traj1[aim_driver][1]: #### dont need last trajectory
-        
-        #cnt += 1
-        #print(cnt)
-        #ct =0
-        
-
-        if len(trajectory)-1 <= number_transition:  ####保证至少一个anchor一个positive
-            continue
-        else:
-            for i in range(len(trajectory)-1-number_transition):
-                
-                
-                if i>move_size:  ###同一个trajectory 向右滑动的最大距离
-                    break
-                
-                
-                
-                anchor = get_subtraj(trajectory, number_transition, i)
-                anchor = torch.tensor(anchor, dtype=torch.float32)
-                
-                # if i + slide_length + number_transition > len(trajectory):
-                #     break
-                
-                positive = get_subtraj(trajectory, number_transition, i + slide_length)  ####slide_length 这个1是滑动间隔，可以考虑设为hyperparameter
-                if len(positive) == 0:
-                    break
-                    
-                
-                positive = torch.tensor(positive, dtype=torch.float32)
-                ###get negative
-                
-
-                ct1 = 0 ##### control negative drivers' number
-                
-                
-                for k in range(len(tagged_traj1)):
-                    
-                    ct1+=1
-                    if ct1>=used_driver_size:
-                        break
-                    
-                    
-                    #print('k',k)
-                    
-                    
-                    
-                    if k != aim_driver:
-                        
-                        
-                        
-                        
-                        for j in tagged_traj1[k][1]:
-                            #print('len j',len(j))
-                            
-                            
-                            random_integer = random.randint(0, 100)  #### 在当前司机下，限制选择的trajectory数量
-                            
-                            
-                            if i + number_transition + 1 < len(j) and random_integer > filter_out:
-                                
-                                negative = get_subtraj(j, number_transition, i)
-                                negative = torch.tensor(negative, dtype=torch.float32)
-                                tuple = (anchor, positive, negative)
-                                
-                                if len(anchor)!= 252 + 127 * (number_transition-1) or len(positive)!= 252 + 127 * (number_transition-1) or len(negative)!= 252 + 127 * (number_transition-1):
-                                    print('error', len(anchor),' ',len(positive),' ',len(negative))
-                                    
-                                    
-                                tuple_list.append(tuple)
-                                
-    return tuple_list     
+      
