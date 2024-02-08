efa4435563eb40b3362d81fc7f73f168e7bdbd3b
anony11sdf <159478732+anony11sdf@users.noreply.github.com>
2024-02-08 13:58:40 -0500

    Update dynamic.py

--- dynamic.py
+++ dynamic.py
@@ -8,23 +8,22 @@ import matplotlib.pyplot as plt
 
 num_epochs=2000
 
-# Device configuration
 device = torch.device('cuda:5' if torch.cuda.is_available() else 'cpu')
 
-# 定义一个子模型
+
 class SubModel_state(nn.Module):
     def __init__(self, input_size, output_size):
         super(SubModel_state, self).__init__()
         self.fc1 = nn.Linear(input_size, 64, dtype=torch.float32)
-        #self.fc2 = nn.Linear(64, 64, dtype=torch.float32)
+       
         self.fc2 = nn.Linear(64, output_size, dtype=torch.float32)
 
     def forward(self, x):
         
-        x = x.to(dtype=torch.float32)  # 确保dtype相同
+        x = x.to(dtype=torch.float32) 
         
         x = torch.relu(self.fc1(x))
-        #x = torch.relu(self.fc2(x))
+     
         x = self.fc2(x)
         
 
@@ -32,28 +31,26 @@ class SubModel_state(nn.Module):
         return x
     
     
-    # 定义一个子模型
 class SubModel_reward(nn.Module):
     def __init__(self, input_size, output_size):
         super(SubModel_reward, self).__init__()
         self.fc1 = nn.Linear(input_size, 32, dtype=torch.float32)
-        #self.fc2 = nn.Linear(64, 64, dtype=torch.float32)
         self.fc2 = nn.Linear(32, output_size, dtype=torch.float32)
 
     def forward(self, x):
         
-        x = x.to(dtype=torch.float32)  # 确保dtype相同
+        x = x.to(dtype=torch.float32) 
         
         x = torch.relu(self.fc1(x))
-        #x = torch.relu(self.fc2(x))
+        
         x = self.fc2(x)
         
-        x = torch.clamp(x, min=-100, max=200)  # 限制输出范围
+        x = torch.clamp(x, min=-100, max=200)  
         
         return x
     
     
-# 动态模型类
+
 class SimpleDynamicModel(nn.Module):
     def __init__(self, state_size, action_size, reward_size, ensemble_size=1):
         super(SimpleDynamicModel, self).__init__()
@@ -63,32 +60,21 @@ class SimpleDynamicModel(nn.Module):
         self.reward_size = reward_size
         self.ensemble_size = ensemble_size
 
-        # 创建多个子模型
+        
         self.ensemble_models = nn.ModuleList([SubModel_state(state_size + action_size, state_size) for _ in range(ensemble_size)])
         self.reward_models = nn.ModuleList([SubModel_reward(state_size + action_size, reward_size) for _ in range(ensemble_size)])
 
     def forward(self, state, action):
-        # 前向传播，对每个子模型进行计算
         
         
-        combined_input = torch.cat([state, action], dim=1)  # 组合状态和动作
+        
+        combined_input = torch.cat([state, action], dim=1)  
         predicted_states = [model(combined_input) for model in self.ensemble_models]
         predicted_rewards = [model(combined_input) for model in self.reward_models]
         return predicted_states, predicted_rewards
         
-        # state = state.to(dtype=torch.float32)  # 确保dtype相同
-        # action = action.to(dtype=torch.float32)  # 确保dtype相同
-        
-        # predicted_states = [model(torch.cat([state, action], dim=1)) for model in self.ensemble_models]
-        # predicted_rewards = [model(torch.cat([state, action], dim=1)) for model in self.reward_models]
+ 
 
-        # return predicted_states, predicted_rewards   
-
-
-
-# 示例使用
-# simple_dynamic_model = SimpleDynamicModel(state_size, action_size).to(device)
-# 预测示例：next_state_reward = simple_dynamic_model(state, action)
 class TransitionDataset(Dataset):
     def __init__(self, transitions):
         self.transitions = transitions
@@ -117,12 +103,12 @@ def train_simple_dynamic_model(model, train_dataloader, optimizer, num_epochs=20
         
         for state_action, next_state_reward in train_dataloader:
             state_action = state_action.to(device)
-            next_state = next_state_reward[:, :-1].to(device)  # 假设最后一列是奖励
-            reward = next_state_reward[:, -1].to(device)  # 假设最后一列是奖励
+            next_state = next_state_reward[:, :-1].to(device)  
+            reward = next_state_reward[:, -1].to(device)  
 
             predicted_states, predicted_rewards = model(state_action[:, :125], state_action[:, 125:])
             
-            # 分别计算状态和奖励的损失
+           
             state_loss = sum(criterion(pred_state, next_state) for pred_state in predicted_states)
             reward_loss = sum(criterion(pred_reward, reward.unsqueeze(1)) for pred_reward in predicted_rewards)
             loss = state_loss + reward_loss
@@ -135,10 +121,9 @@ def train_simple_dynamic_model(model, train_dataloader, optimizer, num_epochs=20
         average_loss = total_loss / len(train_dataloader)
         losses.append(average_loss)
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {average_loss:.4f}')
-        if (epoch + 1) % 1000 == 0:  # 在每100个epoch结束后保存模型
-            torch.save(model.state_dict(), f'simple_dynamic_model_UDS_epoch_{epoch+1}.pth')
 
-    # Plot the training losses
+
+    
     plt.plot(losses, label='Training Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
@@ -148,11 +133,11 @@ def train_simple_dynamic_model(model, train_dataloader, optimizer, num_epochs=20
     plt.savefig('simple_dynamic.png')
     return model
 
-# Load data
-with open('/home/xinbo/project1/data/UDS_full_buffer_20driver.pkl', 'rb') as f:
+
+with open('...', 'rb') as f:
     full_transition_buffer = pickle.load(f)
 
-# Prepare dataset and dataloader
+
 train_dataset = TransitionDataset(full_transition_buffer)
 train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
@@ -161,7 +146,7 @@ train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 # Model initialization
 state_size = 125
 action_size = 1
-reward_size = 1  # 假设奖励的大小是1
+reward_size = 1  
 
 lr = 0.1
 simple_dynamic_model = SimpleDynamicModel(state_size, action_size, reward_size).to(device)
@@ -173,4 +158,4 @@ optimizer = optim.Adam(simple_dynamic_model.parameters(), lr)
 trained_model = train_simple_dynamic_model(simple_dynamic_model, train_dataloader, optimizer, num_epochs)
 
 # Save the trained model
-torch.save(trained_model.state_dict(), 'simple_dynamic_model_UDS.pth')
+torch.save(trained_model.state_dict(), '.....')
