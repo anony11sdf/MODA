0d4d51a51c755a4619f872c7720cbaa14e095cbd
anony11sdf <159478732+anony11sdf@users.noreply.github.com>
2024-02-08 13:53:04 -0500

    Update GAN_model.py

--- GAN_model.py
+++ GAN_model.py
@@ -3,62 +3,6 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import pickle
 
-
-# 定义生成器和判别器
-
-
-
-
-# 定义生成器和鉴别器
-
-# class Generator(nn.Module):
-#     def __init__(self, input_size, output_size):
-#         super(Generator, self).__init__()
-#         self.model = nn.Sequential(
-#             nn.Linear(input_size, 256),
-#             nn.BatchNorm1d(256),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Linear(256, output_size),
-#             nn.Tanh()  # 输出层使用 Tanh 以输出介于 -1 和 1 之间的值
-#         )
-#         self.apply(self._init_weights)
-
-#     def forward(self, x):
-#         return self.model(x)
-    
-#     def _init_weights(self, m):
-#         if isinstance(m, nn.Linear):
-#             nn.init.kaiming_uniform_(m.weight)
-#             if m.bias is not None:
-#                 nn.init.constant_(m.bias, 0)
-
-# class Discriminator(nn.Module):
-#     def __init__(self, input_size):
-#         super(Discriminator, self).__init__()
-#         self.model = nn.Sequential(
-#             nn.Linear(input_size, 256),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Linear(256, 1),
-#             nn.Sigmoid()
-#         )
-#         self.apply(self._init_weights)
-
-#     def forward(self, x):
-#         return self.model(x)
-    
-#     def _init_weights(self, m):
-#         if isinstance(m, nn.Linear):
-#             nn.init.kaiming_uniform_(m.weight)
-#             if m.bias is not None:
-#                 nn.init.constant_(m.bias, 0)
-
-
-
-
-
-
-
-
 class Generator(nn.Module):
     def __init__(self, input_size, output_size):
         super(Generator, self).__init__()
