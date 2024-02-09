da96e0f592ffe6f0bc32c2a963d35777f0e0af76
anony11sdf <159478732+anony11sdf@users.noreply.github.com>
2024-02-08 21:21:57 -0500

    Update train_GAN.py

--- train_GAN.py
+++ train_GAN.py
@@ -26,46 +26,45 @@ class TransitionDataset(Dataset):
     def __getitem__(self, idx):
 
         transition = torch.tensor(self.transitions[idx], dtype=torch.float32)
-        return transition, transition  # 第二个 transition 作为占位符，实际不使用
+        return transition, transition  
 
-# 超参数
-input_size = 100  # 用于生成器的随机噪声向量大小
-output_size = 252  # 生成的transition向量大小
+
+input_size = 100  
+output_size = 252  
 batch_size = 32
 epochs = 5000
 
-# 加载数据集
-with open('/home/xinbo/project1/data/UDS_full_buffer_20driver.pkl', 'rb') as file:
+
+with open('', 'rb') as file:
     buffer = pickle.load(file)
 
-# 创建数据加载器
 train_dataset = TransitionDataset(buffer)
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-# 初始化生成器和判别器
+
 generator = Generator(input_size, output_size).to(device)
 
 discriminator = Discriminator(output_size).to(device)
 
 
 
-# 定义生成器和判别器的优化器
+
 gen_optimizer = torch.optim.Adam(generator.parameters(), lr=0.0005)
 dis_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0002)
 
 
-# 用于记录损失的列表
+
 gen_losses = []
 dis_losses = []
 
 
-# 训练循环
+
 for epoch in range(epochs):
     for real_data, _ in train_dataloader:
         real_data = real_data.to(device)
         batch_size = real_data.size(0)
 
-        # 训练判别器
+
         dis_optimizer.zero_grad()
         labels_real = torch.ones(batch_size, 1).to(device)
         decision_real = discriminator(real_data)
@@ -81,7 +80,7 @@ for epoch in range(epochs):
         dis_loss.backward()
         dis_optimizer.step()
 
-        # 训练生成器
+
         gen_optimizer.zero_grad()
         labels_gen = torch.ones(batch_size, 1).to(device)
         generated_data = generator(noise)
@@ -95,23 +94,12 @@ for epoch in range(epochs):
     gen_losses.append(gen_loss.item())
     dis_losses.append(dis_loss.item())
 
-    print(f'Epoch [{epoch}/{epochs}], 生成器损失: {gen_loss.item():.4f}, 判别器损失: {dis_loss.item():.4f}')
-    if (epoch + 1) % 1000 == 0:  # 在每100个epoch结束后保存模型
-        torch.save(generator.state_dict(), f'GAN_generator_UDS_epoch_{epoch+1}.pth')
-        torch.save(discriminator.state_dict(), f'GAN_discriminator_UDS_epoch_{epoch+1}.pth')
 
 
-torch.save(generator.state_dict(), 'generator_model17_UDS.pth')
-torch.save(discriminator.state_dict(), 'discriminator_model17_UDS.pth')
 
-# 绘制生成器和判别器的损失折线图
-plt.plot(gen_losses, label='Generator Loss')
-plt.plot(dis_losses, label='Discriminator Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('gan_loss_plot.png')
-plt.show()
+torch.save(generator.state_dict(), '')
+torch.save(discriminator.state_dict(), '')
 
 
-#########使用判别器时需要阈值
+
+
